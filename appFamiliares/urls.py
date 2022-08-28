9a6ac0da542d91c2d4ec2f74901f73e293a602da
YoanaGerling <yoanagerling@gmail.com>
2022-08-28 12:21:01 -0300

    Agregue templete

--- appFamiliares/urls.py
+++ appFamiliares/urls.py
@@ -2,6 +2,7 @@ from appFamiliares.views import *
 from django.urls import path
 
 urlpatterns = [
-    path('familiares/', familiares, name='familiares'),
     path('inicio/', inicio),
+    path('familiares/', familiares),
+    
 ]
--- appFamiliares/views.py
+++ appFamiliares/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Familiares 
 from django.http import HttpResponse 
 
+
 def familiares(request):
     familiar1 = Familiares(nombre = 'Julian', edad = 38, cumpleanios = '1986-07-02' )
     familiar2 = Familiares(nombre = 'Ernesto', edad = 62, cumpleanios = '1960-10-21')
@@ -9,11 +10,9 @@ def familiares(request):
     familiar1.save()
     familiar2.save()
     familiar3.save()
-    texto1 = f'HERMANO: {familiar1.nombre}, de {familiar1.edad} anios y fecha de nacimiento {familiar1.cumpleanios}'
-    texto2 = f'PAPA: {familiar2.nombre}, de {familiar2.edad} anios y fecha de nacimiento {familiar2.cumpleanios}'
-    texto3 = f'MAMA: {familiar3.nombre}, de {familiar3.edad} anios y fecha de nacimiento {familiar3.cumpleanios}'
-    return HttpResponse(texto1, texto2, texto3)
+    texto= 'Familiares creados'
+    return HttpResponse(texto)
     
 
 def inicio(request): 
-    return render (request, 'appFamiliares/inicio.html')
+    return render (request, "appFamiliares/inicio.html")
